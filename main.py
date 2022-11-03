e813bd7dbcb6d535abfd6f476644df559533b7e6
Alex <61087802+alexbraic@users.noreply.github.com>
2022-11-03 22:43:49 +0000

    Added statement

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+# simple string
+print("\tSingle-line string:")
 print("Hello Replit")
+
+# multi-line string
+print("\tMulti-line string:")
 print(""" 
 BIT
 OF
 TEXT
+""")
+
+# declaration of "I'm surelly going to forget about this in a while 🤣"
+print("\tWell ma'am, I do declare:")
+print("""
+Alex
+November 3rd 2022
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum 10 minutes a day.
+I'll be using Replit, an amazing online IDE so i can do this from my phone wherever i happen to be. No excuses for not coding from the middle of a field!
+I am feeling 😂
+You can follow my progress at replit.com/@AlexB78
 """)
