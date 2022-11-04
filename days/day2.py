d98cc2516dd8e984babc54438b7a7009fc3b3768
Alex <61087802+alexbraic@users.noreply.github.com>
2022-11-04 15:56:14 +0000

    completed day2 challenge and added it to the day2.py file

--- days/day2.py
+++ days/day2.py
@@ -2,6 +2,7 @@
 # variables and user input
 user_name = input("What is your name?: ")
 print("Hi, ", user_name)
+
 # day 2 challenge
 # get user name, where they live, favourite food, favourite music, all stored in variables
 username = input("What's your name?: ")
@@ -12,3 +13,9 @@ fav_music = input("What is your favourite music?: ")
 print()
 user_location = input("Where do you live?: ")
 print()
+
+# print statement about user
+print("You are \n", username)
+print("you're probably hungry for \n", fav_food)
+print("and you're definitely getting your groove on to \n", fav_music)
+print("while living in amazing \n", user_location)
