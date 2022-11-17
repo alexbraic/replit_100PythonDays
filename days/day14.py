dacb72f8e8dfb138d32376d2280f154508ec6341
Alex <61087802+alexbraic@users.noreply.github.com>
2022-11-17 22:10:28 +0000

    Added day15.py adventure game moved from main.py

--- days/day14.py
+++ days/day14.py
@@ -7,27 +7,30 @@ print()
 """rock smashes scissors,
 paper covers rock,
 scissors cut paper"""
+again = "y"
+while again.lower(): 
+  if p1 == "rock" and p2 == "scissors":
+    print("Player1's rock smashes Player2's scissors!")
+    print("Player1 wins!")
+  elif p1 == "rock" and p2 == "paper":
+    print("Player2's paper covers Player1's rock!")
+    print("Player2 wins!")
+  
+  if p1 == "paper" and p2 == "scissors":
+    print("Player2's scissors cuts Player1's paper!")
+    print("Player2 wins!")
+  elif p1 == "paper" and p2 == "rock":
+    print("Player1's paper covers Player2's rock!")
+    print("Player1 wins!")
+  
+  if p1 == "scissors" and p2 == "paper":
+    print("Player1's scissors cuts Player2's paper!")
+    print("Player2 wins!")
+  elif p1 == "scissors" and p2 == "rock":
+    print("Player2's rock smashes Player1's scissors!")
+    print("Player2 wins!")
+  
+  if p1 == p2:
+    print("It's a draw!")
 
-if p1 == "rock" and p2 == "scissors":
-  print("Player1's rock smashes Player2's scissors!")
-  print("Player1 wins!")
-elif p1 == "rock" and p2 == "paper":
-  print("Player2's paper covers Player1's rock!")
-  print("Player2 wins!")
-
-if p1 == "paper" and p2 == "scissors":
-  print("Player2's scissors cuts Player1's paper!")
-  print("Player2 wins!")
-elif p1 == "paper" and p2 == "rock":
-  print("Player1's paper covers Player2's rock!")
-  print("Player1 wins!")
-
-if p1 == "scissors" and p2 == "paper":
-  print("Player1's scissors cuts Player2's paper!")
-  print("Player2 wins!")
-elif p1 == "scissors" and p2 == "rock":
-  print("Player2's rock smashes Player1's scissors!")
-  print("Player2 wins!")
-
-if p1 == p2:
-  print("It's a draw!")
+  again = input("Go again? (y or n)")
