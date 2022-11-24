c8ead000a7771a21c024cda59c185f793fcaed53
Alex <61087802+alexbraic@users.noreply.github.com>
2022-11-24 17:26:23 +0000

    Added day22.py - making use of random

---
+++ days/day22.py
@@ -0,0 +1,21 @@
+# bit random
+import random
+
+myNumber = random.randint(1, 100)
+for i in range(10):
+  myNumber = random.randint(1, 100)
+  print(myNumber)
+print()
+for i in range(10):
+  x = random.random()
+  print(x)
+print()
+print(random.randint(5,10))
+c = [1, 2, 3]
+print()
+
+def repeat(x, rep=0):
+  for i in range(rep):
+    print(x)
+
+repeat("Alex", random.randint(1, 15))
