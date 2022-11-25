f95b294f1b8709c877b25ee4478198d52d064ef3
Alex <61087802+alexbraic@users.noreply.github.com>
2022-11-25 18:04:41 +0000

    added day23.py with a simple login function

---
+++ days/day23.py
@@ -0,0 +1,45 @@
+# subroutines
+#def rollDice():
+# import random
+# dice = random.randint(1, 6)
+# print("You rolled", dice)
+
+#rollDice()
+
+# login system
+
+# hard coded user and password
+user = "Joe"
+passwd = "passwd"
+
+
+# define simple login logic
+def login():
+    uname = input("Username: ")
+    upass = input("Password: ")
+
+    while uname != user or upass != passwd:
+        print("Wrong credentials, please try agin")
+        uname = input("Username: ")
+        upass = input("Password: ")
+
+
+# first line, print welcome message
+print("Welcome, please log in.")
+
+# ask user to log in, and display a log in msg once successful login
+loggedin = False
+while loggedin == False:
+    login()
+    loggedin = True
+
+print(f"Welcome back {user}!")
+
+
+# will look at this tomorrow
+def newUser():
+    newUname = input("Enter username: ")
+    newUpass = input("Enter password: ")
+
+
+users = {"username": "Joe", "password": "passwd"}
