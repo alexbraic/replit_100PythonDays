c2d608f8c5d23f0a3b2dc3cd8d6a1eecaf191c91
Alex <61087802+alexbraic@users.noreply.github.com>
2022-11-28 23:04:10 +0000

    day26.py improved menu but still needs work

--- days/day26.py
+++ days/day26.py
@@ -5,7 +5,7 @@ import os, time
 #   os.system("clear")
 
 #title list and playlists
-titles = [" MyPOD Music Player ", " Your playlists "]
+titles = [" MyPOD Music Player ", " Main Menu ", " Your playlists "]
 playlist = ["Skipping Time", "Classic Rock"]
 
 print(titles[0].center(60, "="))
@@ -13,35 +13,48 @@ time.sleep(1)
 os.system("clear")
 
 #print main menu
-#def main_menu():
-print("\tPress 1 to see Playlists")
-print("\tPress 2 to Exit")
-print("Press anything else to see the menu again")
+def main_menu():
+  print(titles[1].center(60, "="))
+  print("\tPress 1 to see Playlists")
+  print("\tPress 2 to Exit")
+  print()
+  print("Press anything else to see the menu again")
+
+def play_lists():
+  print(titles[2].center(60, "="))
+  print(f"\t1: {playlist[0]}")
+  print(f"\t2: {playlist[1]}")
+  print()
+  print("To return to the Main Menu, press 0.")
+main_menu()
 press = int(input(""))
-  #return press
-  
-#main_menu()
 choice = "0"
-while True:
+os.system("clear")
+while press != 2:
   if press == 1:
-    #os.system("clear")
-    print(titles[1].center(60, "="))
-    print(f"\t1: {playlist[0]}")
-    print(f"\t2: {playlist[1]}")
-    choice = input("")
-    if choice == "1":
-      print(f'Playing {playlist[0]}')
-      continue
-    if choice == "2":
-      print(f'Playing {playlist[1]}')
-      continue
-    if choice == "0":
+    play_lists()
+    p_list = int(input())
+    os.system("clear")
+    if p_list == 1:
+      print(f'Now playing {playlist[0]}. Enjoy!\n')
+      ch = input("To return to playlists, press 0.\n> ")
+      os.system("clear")
+      if ch == int(0):
+        play_lists()
+        os.system("clear")
+        #continue
+    if p_list == 2:
+      print(f'Now playing {playlist[1]}. Enjoy!\n')
+      ch = input("To return to playlists, press 0.\n> ")
+      os.system("clear")
+      if ch == int(0):
+        play_lists()
+        os.system("clear")
+    if p_list == 0:
       main_menu()
-  elif press == 2:
-    print("Goodbye")
-    break
+      continue
+    
+  
   else:
-    print("\tPress 1 to see Playlists")
-    print("\tPress 2 to Exit")
-    print("Press anything else to see the menu again")
+    main_menu()
     press = int(input(""))
