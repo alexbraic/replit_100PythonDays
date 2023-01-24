c56bff3aa8ec4ffc523efe626d1982b912ac6441
Alex <61087802+alexbraic@users.noreply.github.com>
2023-01-24 21:05:36 +0000

    Added day83.py with 2 themes added to the 'blog', as GET args

---
+++ days/day83.py
@@ -0,0 +1,59 @@
+from flask import Flask, request
+
+app = Flask(__name__, static_url_path='/static')
+
+my_reflections = {}
+
+my_reflections["77"] = {
+  'day':
+  "77",
+  "link":
+  "https://replit.com/@AlexB78/Day77100Days#main.py",
+  'reflection':
+  "I really should give more attention to this Flask thing. Maybe read more about it, or check out more projects built around it."
+}
+
+my_reflections["78"] = {
+  'day':
+  "78",
+  'link':
+  "https://replit.com/@AlexB78/Day78100Days#main.py",
+  'reflection':
+  "Programming can be really demotivating when it doesn't go the way you want it to go, but keep at it, it might lead to something good."
+}
+
+
+@app.route('/')
+def index():
+  page = f"""<html>
+              <body>
+                <p><a href="/77">77</a></p>
+                <p><a href="/78">78</a></p>
+              </body>
+            </html>
+  """
+  return page
+
+
+@app.route('/<page_number>', methods=["GET"])
+def number(page_number):
+  get = request.args
+  style = "regular"
+  if get != {}:
+    style = get["style"]
+
+  page = ""
+  f = open("templates/base_index.html", "r")
+  page = f.read()
+  f.close()
+
+  page = page.replace("{day}", page_number)
+  page = page.replace("{link}", my_reflections[page_number]["link"])
+  page = page.replace("{reflection}",
+                      my_reflections[page_number]['reflection'])
+  page = page.replace("{style}", style)
+
+  return page
+
+
+app.run(host='0.0.0.0', port=81)
